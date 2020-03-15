69570d5c0ff49aca86d28689e083de476b079969
meenakshisund <meenakshisundaraja@gmail.com>
2020-03-15 21:20:35 +0530

    Added test case

--- src/main/java/com/webservice/call/IfscResponse.java
+++ src/main/java/com/webservice/call/IfscResponse.java
@@ -49,4 +49,116 @@ class IfscResponse implements Serializable {
 
     @JsonProperty("IMPS")
     Boolean imps;
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getCentre() {
+        return centre;
+    }
+
+    public void setCentre(String centre) {
+        this.centre = centre;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMicrCode() {
+        return micrCode;
+    }
+
+    public void setMicrCode(String micrCode) {
+        this.micrCode = micrCode;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getIfscCode() {
+        return ifscCode;
+    }
+
+    public void setIfscCode(String ifscCode) {
+        this.ifscCode = ifscCode;
+    }
+
+    public Boolean getUpi() {
+        return upi;
+    }
+
+    public void setUpi(Boolean upi) {
+        this.upi = upi;
+    }
+
+    public Boolean getRtgs() {
+        return rtgs;
+    }
+
+    public void setRtgs(Boolean rtgs) {
+        this.rtgs = rtgs;
+    }
+
+    public Boolean getNeft() {
+        return neft;
+    }
+
+    public void setNeft(Boolean neft) {
+        this.neft = neft;
+    }
+
+    public Boolean getImps() {
+        return imps;
+    }
+
+    public void setImps(Boolean imps) {
+        this.imps = imps;
+    }
 }
--- src/test/java/com/webservice/call/WebserviceCallApplicationTests.java
+++ src/test/java/com/webservice/call/WebserviceCallApplicationTests.java
@@ -1,13 +1,36 @@
 package com.webservice.call;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class WebserviceCallApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private WebServiceController webServiceController;
 
+	@MockBean
+	WebService webService;
+
+	@DisplayName("actual rest call")
+	@Test
+	void test1() throws URISyntaxException {
+		IfscResponse response = new IfscResponse();
+		response.setIfscCode("HDFC0001852");
+		ResponseEntity<IfscResponse> responseEntity = new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+		when(webServiceController.getDetailsByIfscCode("HDFC0001852"))
+				.thenReturn(responseEntity);
+		assert responseEntity.getStatusCode() == HttpStatus.ACCEPTED;
+		assert Objects.requireNonNull(responseEntity.getBody()).getIfscCode().equals("HDFC0001852");
+	}
 }
